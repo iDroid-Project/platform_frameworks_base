da27e7c394c7c0800d737158765e402e76ac871b
Nick Pack <nick@nickpack.com>
2011-05-19 19:39:29 +0100

    SD Hack - This should be done using the now built-in sd emulation, however some time needs to be spent on this, as I'm not sure the loopbacks are helping us get that working...

--- core/java/android/os/Environment.java
+++ core/java/android/os/Environment.java
@@ -381,7 +381,7 @@ public class Environment {
      * <p>See {@link #getExternalStorageDirectory()} for more information.
      */
     public static String getExternalStorageState() {
-        try {
+/*        try {
             if (mMntSvc == null) {
                 mMntSvc = IMountService.Stub.asInterface(ServiceManager
                                                          .getService("mount"));
@@ -389,8 +389,9 @@ public class Environment {
             return mMntSvc.getVolumeState(getExternalStorageDirectory().toString());
         } catch (Exception rex) {
             return Environment.MEDIA_REMOVED;
-        }
-    }
+        } */
+    return SystemProperties.get("EXTERNAL_STORAGE_STATE", MEDIA_REMOVED);
+	}
 
     /**
      * Returns whether the primary "external" storage device is removable.
